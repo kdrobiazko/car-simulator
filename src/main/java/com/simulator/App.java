0610f861ced0158fb71d38f21bca9ae856d80d3f
Kostiantyn Drobiazko <kostiantyn.drobiazko_ges@omio.com>
2020-04-07 15:14:33 +0300

    Configured logging

--- src/main/java/com/simulator/App.java
+++ src/main/java/com/simulator/App.java
@@ -1,8 +1,10 @@
 package com.simulator;
 
-/** Hello world! */
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    log.info("Hello World!");
   }
 }
